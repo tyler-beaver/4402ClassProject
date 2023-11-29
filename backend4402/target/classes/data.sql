125259bb0feb470ffa8c78abece8089c8b01c366
tbeaver23 <tbeaver23@yahoo.com>
2023-11-28 21:14:52 -0600

    update

--- backend4402/target/classes/data.sql
+++ backend4402/target/classes/data.sql
@@ -1,15 +1,15 @@
 -- This file will populate the database with sample data.
 -- You will need to create a sufficient amount of sample data (many INSERT statements) for your tables.
 
-INSERT INTO customer (customer_id, first_name, last_name, phone_number, customer_license_num)
-VALUES (43424, 'Adriana', 'Garner', 3452071789, 2182781718);
+INSERT INTO customer (first_name, last_name, phone_number, customer_license_num)
+VALUES ('Adriana', 'Garner', '3452071789', '2182781718');
 
--- INSERT INTO employee (employee_id, first_name, last_name, role, salary)
--- VALUES (30204, 'Lucie', 'Olson', 'Accounting', 99637.00);
---
--- INSERT INTO reservation (reservation_id, customer_id, employee_id, vehicle_vin, start_date, end_date, cost)
--- VALUES (00355, 12629, 16726, 50000, '06/26/2025', '06/26/2026', 544);
---
--- INSERT INTO vehicle (vin, license_id, make, model, year, reservation_id)
--- VALUES (9832904, 328382, 'Kia', 'Rio', '1992', 66070);
+INSERT INTO employee (first_name, last_name, role, salary)
+VALUES ('Lucie', 'Olson', 'Accounting', '99637.00');
+
+INSERT INTO reservation (customer_id, employee_id, vehicle_vin, start_date, end_date, cost)
+VALUES (12629, 16726, 50000, '06/26/2025', '06/26/2026', 544);
+
+INSERT INTO vehicle (license_id, make, model, "year", reservation_id)
+VALUES (328382, 'Kia', 'Rio', 1992, 66070);
 -- ADD INSERT STATEMENTS FOR YOUR RELATIONS HERE
--- backend4402/target/classes/schema.sql
+++ backend4402/target/classes/schema.sql
@@ -5,35 +5,35 @@ CREATE TABLE customer (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    phone_number INT(10),
-    customer_license_num INT(10)
+    phone_number VARCHAR(20),
+    customer_license_num VARCHAR(20)
 );
 
--- CREATE TABLE employee (
---     employee_id INT AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(50),
---     last_name VARCHAR(50),
---     role VARCHAR(50),
---     salary DECIMAL(10, 2)
--- );
+CREATE TABLE employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    role VARCHAR(50),
+    salary DECIMAL(10, 2)
+);
 
--- CREATE TABLE reservation (
---     reservation_id INT AUTO_INCREMENT PRIMARY KEY,
---     customer_id INT(10),
---     employee_id INT(10),
---     vehicle_vin INT(10),
---     start_date VARCHAR(50),
---     end_date VARCHAR(50),
---     cost DECIMAL(10, 2)
--- );
+CREATE TABLE reservation (
+    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    employee_id INT,
+    vehicle_vin INT,
+    start_date VARCHAR(50),
+    end_date VARCHAR(50),
+    cost DECIMAL(10, 2)
+);
 
--- CREATE TABLE vehicle (
---     vin INT AUTO_INCREMENT PRIMARY KEY,
---     license_id INT(10),
---     make VARCHAR(50),
---     model VARCHAR(50),
---     year INT(10),
---     reservation_id INT(10)
--- );
+CREATE TABLE vehicle (
+    vin INT AUTO_INCREMENT PRIMARY KEY,
+    license_id INT,
+    make VARCHAR(50),
+    model VARCHAR(50),
+    "year" INT,
+    reservation_id INT
+);
 
 -- DEFINE YOUR TABLES HERE
