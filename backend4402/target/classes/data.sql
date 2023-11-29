396a85e49e4a0a61d0e3bf244fd4d92b8fed7baa
tbeaver23 <tbeaver23@yahoo.com>
2023-11-28 21:15:12 -0600

    udpate

--- backend4402/target/classes/data.sql
+++ backend4402/target/classes/data.sql
@@ -1,15 +1,7 @@
 -- This file will populate the database with sample data.
 -- You will need to create a sufficient amount of sample data (many INSERT statements) for your tables.
 
-INSERT INTO customer (first_name, last_name, phone_number, customer_license_num)
-VALUES ('Adriana', 'Garner', '3452071789', '2182781718');
+INSERT INTO Employee (FirstName, LastName, Department, Salary)
+VALUES ('John', 'Doe', 'Sales', 50000.00);
 
-INSERT INTO employee (first_name, last_name, role, salary)
-VALUES ('Lucie', 'Olson', 'Accounting', '99637.00');
-
-INSERT INTO reservation (customer_id, employee_id, vehicle_vin, start_date, end_date, cost)
-VALUES (12629, 16726, 50000, '06/26/2025', '06/26/2026', 544);
-
-INSERT INTO vehicle (license_id, make, model, "year", reservation_id)
-VALUES (328382, 'Kia', 'Rio', 1992, 66070);
 -- ADD INSERT STATEMENTS FOR YOUR RELATIONS HERE
