59416343e413748434e55ce12bf2aa1c1dd1ef57
tbeaver23 <tbeaver23@yahoo.com>
2023-11-28 20:24:17 -0600

    update
    backend

--- backend4402/target/classes/schema.sql
+++ backend4402/target/classes/schema.sql
@@ -1,10 +1,10 @@
 -- Example file that defines the schema for the database.
 -- All relations will be defined here, like the one below.
 
-CREATE TABLE Employee (
-    EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
+CREATE TABLE customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     Department VARCHAR(50),
     Salary DECIMAL(10, 2)
 );
